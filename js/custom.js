97203ed9088003d1e72b102a4c3653442f18fba7
simotae14 <startae14@gmail.com>
2017-08-31 19:44:11 +0200

    Aggiunto owl-carousel alla sezione Testimonials

--- js/custom.js
+++ js/custom.js
@@ -32,3 +32,16 @@ $(function () {
     autoplayHoverPause: true
   });
 });
+
+/*******************************************
+            TESTIMONIALS
+*******************************************/
+$(function () {
+  $("#customers-testimonials").owlCarousel({
+    items: 1,
+    autoplay: true,
+    smartSpeed: 700,
+    loop: true,
+    autoplayHoverPause: true
+  });
+});
