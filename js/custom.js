7628cf1e739f5b497399ca8e8783078feeb02f64
simotae14 <startae14@gmail.com>
2017-09-05 15:43:19 +0200

    Stile responsive per medium devices, part 2

--- js/custom.js
+++ js/custom.js
@@ -101,3 +101,10 @@ $(function () {
     }, 1250, "easeInOutExpo");
   })
 });
+
+// Close Mobile Menu on click
+$(function(){
+  $('.navbar-collapse ul li a').on('click touch', function(){
+    $('.navbar-toggle').click();
+  });
+});
